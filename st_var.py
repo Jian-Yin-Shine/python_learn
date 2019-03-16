33fa31b8a11a79f7b85b89ca9a94683745b7d915
= <=>
2019-03-16 18:39:25 +0800

    fun_var

--- st_var.py
+++ st_var.py
@@ -43,9 +43,30 @@ def fun(*arg1, **vardict):
         print(i, vardict[i])
 fun(1,2,3,a=1,b=2)
 
+# 参数传递，（值传递，引用传递）
+def fun(a):
+    a = 10
+b = 2
+fun(b)
+# 有int对象2，指向它的变量是b，传给函数时，按传值的方式复制了变量b，a,b同时指向2
+# a = 10 ，新建一个int对象 10, a指向它，所以b不变
+
+def fun(mylist):
+    mylist.append([1,2,3,4])
+    print(mylist)
+
+ls = [10,20]
+fun(ls)
+# [10, 20, [1, 2, 3, 4]]
+>>> ls
+# [10, 20, [1, 2, 3, 4]]
+
 
 # 匿名函数
 ums = lambda arg1, *arg2 : arg1 + sum(arg2)
-sums(1,1,2)    
+sums(1,1,2)
+
+
+
 
 
