a94a705a33185f2b6d3eb81bc00151bb181de972
Jian-Yin-Shine <1262094391@qq.com>
2019-04-26 14:07:33 +0800

    argparse

--- module/st_argparse.py
+++ module/st_argparse.py
@@ -1,1 +1,14 @@
 import argparse
+
+
+def parse_arg():
+    parser = argparse.ArgumentParser(description="abc")
+    parser.add_argument('--num', type=int, default=1)
+    parser.add_argument('--gpu', type=str, default='0')
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_arg()
+    print(args.num, args.gpu)
